fec75f2889ff4e1f48200543b8376177e0c8d058
Erik Römmelt <roemmelter63329@th-nuernberg.de>
2017-05-18 10:10:48 +0200

    add WebView

--- app/src/main/java/yow/yourownway/MainActivity.java
+++ app/src/main/java/yow/yourownway/MainActivity.java
@@ -22,22 +22,24 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private WebView mWebView;
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new MyAppWebViewClient());
+        mWebView.loadUrl("https://www.google.com");
+
         // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
-        mWebView = (WebView) findViewById(R.id.activity_main_webview);
-        mWebView.loadUrl("https://www.google.de/");
-*/
-
+/*
         List<CalendarProvider> availableCalenders = getAvailableCalenders();
 
         Calendar start = Calendar.getInstance();
@@ -48,21 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
         List<EventProvider> availableEvents = findEventsInPeriod(start, end);
 
-        // Debuging
-        /*
-            Output:
-            Fronleichnam
-            15.6.
-            16.6.
-            -> ???
-         */
-        while (availableEvents.iterator().hasNext() ) {
-            EventProvider event = availableEvents.iterator().next();
-            Log.d("event", event.title);
-            Log.d("description", event.description);
-            Log.d("startDate", event.start.toString());
-            Log.d("endDate", event.end.toString());
-        }
     }
 
     public List<EventProvider> findEventsInPeriod(Calendar begin, Calendar end) {
@@ -136,6 +123,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             throw (new RuntimeException("No permissions"));
         }
+*/
+/*
+        public void onBackPressed() {
+            if(mWebView.canGoBack()) {
+                mWebView.goBack();
+            } else {
+                super.onBackPressed();
+            }
+        }
+*/
 
     }
 }
